3e7848a06720ac9edaad5becfe6a692497886828
doicrm <doic2701@gmail.com>
2025-06-10 22:18:52 +0200

    added macros for define externals func

--- src/DSP_Externals/Externals_Npcs.cpp
+++ src/DSP_Externals/Externals_Npcs.cpp
@@ -56,16 +56,4 @@ namespace GOTHIC_ENGINE {
 			npc->Equip(item);
 		return 0;
 	}
-
-	void DefineNpcExternals()
-	{
-		// func string Npc_GetRoutineName(var C_NPC npc)
-		parser->DefineExternal("Npc_GetRoutineName", Npc_GetRoutineName, zPAR_TYPE_STRING, zPAR_TYPE_INSTANCE, 0);
-		// func string Npc_IsInRoutineName(var C_NPC npc, var string routine)
-		parser->DefineExternal("Npc_IsInRoutineName", Npc_IsInRoutineName, zPAR_TYPE_INT, zPAR_TYPE_INSTANCE, zPAR_TYPE_STRING, 0);
-		// func C_Item Npc_GetSlotItem(var C_NPC npc)
-		parser->DefineExternal("Npc_GetSlotItem", Npc_GetSlotItem, zPAR_TYPE_INSTANCE, zPAR_TYPE_INSTANCE, zPAR_TYPE_STRING, 0);
-		// func void Npc_EquipItem(var C_NPC npc, var int itemInstance)
-		parser->DefineExternal("Npc_EquipItem", Npc_EquipItem, zPAR_TYPE_VOID, zPAR_TYPE_INSTANCE, zPAR_TYPE_INT, 0);
-	}
 }
--- src/DSP_Externals/Plugin.cpp
+++ src/DSP_Externals/Plugin.cpp
@@ -80,99 +80,125 @@ namespace GOTHIC_ENGINE {
   }
   
   void Game_DefineExternals() {
+
+#define INT  zPAR_TYPE_INT
+#define FLT  zPAR_TYPE_FLOAT
+#define INST zPAR_TYPE_INSTANCE
+#define FUNC zPAR_TYPE_FUNC
+#define STR  zPAR_TYPE_STRING
+#define VOID zPAR_TYPE_VOID
+
       //---------------------------------------------------------------------
       // MENU
       //---------------------------------------------------------------------
 
       // func void Menu_SetItemText(var string menuItemName, var string value)
-      parser->DefineExternal("Menu_SetItemText", Menu_SetItemText, 
-          zPAR_TYPE_VOID, zPAR_TYPE_STRING, zPAR_TYPE_STRING, 0);
-      parserMenu->DefineExternal("Menu_SetItemText", Menu_SetItemText, 
-          zPAR_TYPE_VOID, zPAR_TYPE_STRING, zPAR_TYPE_STRING, 0);
+      parser->DefineExternal("Menu_SetItemText", Menu_SetItemText, VOID, STR, STR, 0);
+      parserMenu->DefineExternal("Menu_SetItemText", Menu_SetItemText, VOID, STR, STR, 0);
 
       // func string Menu_GetItemText(var string menuItemName)
-      parser->DefineExternal("Menu_GetItemText", Menu_GetItemText,
-          zPAR_TYPE_STRING, zPAR_TYPE_STRING, 0);
-      parserMenu->DefineExternal("Menu_GetItemText", Menu_GetItemText,
-          zPAR_TYPE_STRING, zPAR_TYPE_STRING, 0);
+      parser->DefineExternal("Menu_GetItemText", Menu_GetItemText, STR, STR, 0);
+      parserMenu->DefineExternal("Menu_GetItemText", Menu_GetItemText, STR, STR, 0);
 
-      parserMenu->DefineExternal("Hlp_StrCmp", Hlp_StrCmp,
-          zPAR_TYPE_INT, zPAR_TYPE_STRING, zPAR_TYPE_STRING, 0);
+      parserMenu->DefineExternal("Hlp_StrCmp", Hlp_StrCmp, INT, STR, STR, 0);
 
       //---------------------------------------------------------------------
       // LOG
       //---------------------------------------------------------------------
 
       // func int Log_GetTopicStatus(var string logName) 
-      parser->DefineExternal("Log_GetTopicStatus", Log_GetTopicStatus,
-          zPAR_TYPE_INT, zPAR_TYPE_STRING, 0);
-      //parser->DefineExternal("DIA_AddChoice", DIA_AddChoice, zPAR_TYPE_VOID, zPAR_TYPE_STRING, zPAR_TYPE_FUNC, 0);
+      parser->DefineExternal("Log_GetTopicStatus", Log_GetTopicStatus, INT, STR, 0);
+      //parser->DefineExternal("DIA_AddChoice", DIA_AddChoice, VOID, STR, FUNC, 0);
 
       //---------------------------------------------------------------------
       // NPC
       //---------------------------------------------------------------------
 
-      DefineNpcExternals();
+      // func string Npc_GetRoutineName(var C_NPC npc)
+      parser->DefineExternal("Npc_GetRoutineName", Npc_GetRoutineName, STR, INST, 0);
+      // func int Npc_IsInRoutineName(var C_NPC npc, var string routine)
+      parser->DefineExternal("Npc_IsInRoutineName", Npc_IsInRoutineName, INT, INST, STR, 0);
+      // func C_Item Npc_GetSlotItem(var C_NPC npc)
+      parser->DefineExternal("Npc_GetSlotItem", Npc_GetSlotItem, INST, INST, STR, 0);
+      // func void Npc_EquipItem(var C_NPC npc, var int itemInstance)
+      parser->DefineExternal("Npc_EquipItem", Npc_EquipItem, VOID, INST, INT, 0);
 
       //---------------------------------------------------------------------
       // Item
       //---------------------------------------------------------------------
 
       // func C_Item Hlp_GetItem(var int itemInstance)
-      parser->DefineExternal("Hlp_GetItem", Hlp_GetItem, 
-          zPAR_TYPE_INSTANCE, zPAR_TYPE_INT, 0);
+      parser->DefineExternal("Hlp_GetItem", Hlp_GetItem, INST, INT, 0);
 
       //---------------------------------------------------------------------
       // WLD
       //---------------------------------------------------------------------
 
-      parser->DefineExternal("Wld_PlayEffectOnVob", Wld_PlayEffectOnVob, 
-          zPAR_TYPE_VOID, zPAR_TYPE_STRING, zPAR_TYPE_INSTANCE, zPAR_TYPE_INT, zPAR_TYPE_INT, zPAR_TYPE_INT, zPAR_TYPE_INT, 0);
-      parser->DefineExternal("Wld_PlayEffectAt", Wld_PlayEffectAt, 
-          zPAR_TYPE_VOID, zPAR_TYPE_STRING, zPAR_TYPE_INSTANCE, zPAR_TYPE_INT, zPAR_TYPE_INT, zPAR_TYPE_INT, zPAR_TYPE_INT, 0);
+      parser->DefineExternal("Wld_PlayEffectOnVob", Wld_PlayEffectOnVob, VOID, STR, INST, INT, INT, INT, INT, 0);
+      parser->DefineExternal("Wld_PlayEffectAt", Wld_PlayEffectAt, VOID, STR, INST, INT, INT, INT, INT, 0);
       // func string Wld_GetPlayerPortalRoom()
-      parser->DefineExternal("Wld_GetPlayerPortalRoom", Wld_GetPlayerPortalRoom,
-          zPAR_TYPE_STRING, 0);
+      parser->DefineExternal("Wld_GetPlayerPortalRoom", Wld_GetPlayerPortalRoom, STR, 0);
 
       //---------------------------------------------------------------------
       // VOB
       //---------------------------------------------------------------------
 
-      DefineVobExternals();
+      // func void Wld_InsertVob(var string vobName, var string visualName, var string point, var int isCollDet)
+      parser->DefineExternal("Wld_InsertVob", Wld_InsertVob, VOID, STR, STR, STR, INT, 0);
+      // func void Wld_InsertVobPos(var string vobName, var string visualName, var int posx, var int posy, var int posz, var int isCollDet)
+      parser->DefineExternal("Wld_InsertVobPos", Wld_InsertVobPos, VOID, STR, STR, INT, INT, INT, INT, 0);
+      // func int Wld_RemoveVob(var string vobName)
+      parser->DefineExternal("Wld_RemoveVob", Wld_RemoveVob, INT, STR, 0);
+      // func void Vob_Rotate(var string vobName, var int posx, var int posy, var int posz)
+      parser->DefineExternal("Vob_Rotate", Vob_Rotate, VOID, STR, INT, INT, INT, 0);
+      // func void Vob_SetVisual(var string vobName, var string visualName)
+      parser->DefineExternal("Vob_SetVisual", Vob_SetVisual, VOID, STR, STR, 0);
+      // func void Vob_ClearVisual(var string vobName)
+      parser->DefineExternal("Vob_ClearVisual", Vob_ClearVisual, VOID, STR, 0);
+      // func void Vob_SetName(var string oldVobName, var string newVobName)
+      parser->DefineExternal("Vob_SetName", Vob_SetName, VOID, STR, STR, 0);
+      // func void Vob_MoveTo(var string vobName, var string point)
+      parser->DefineExternal("Vob_MoveTo", Vob_MoveTo, VOID, STR, STR, 0);
+      // func void Vob_MoveToPos(var string vobName, var int posx, var int posy, var int posz)
+      parser->DefineExternal("Vob_MoveToPos", Vob_MoveToPos, VOID, STR, INT, INT, INT, 0);
+      // func int Vob_SearchByName(var string vobName)
+      parser->DefineExternal("Vob_SearchByName", Vob_SearchByName, VOID, STR, 0);
+      // func void Wld_InsertMobInter(var string vobName, var string visualName, var string point)
+      parser->DefineExternal("Wld_InsertMobInter", Wld_InsertMobInter, VOID, STR, STR, STR, 0);
+      // func void Wld_InsertMobContainer(var string vobName, var string visualName, var string point)
+      parser->DefineExternal("Wld_InsertMobContainer", Wld_InsertMobContainer, VOID, STR, STR, STR, 0);
 
       //---------------------------------------------------------------------
       // AI
       //---------------------------------------------------------------------
 
-      // func string AI_TurnToWP(var C_NPC npc, var string wp)
-      parser->DefineExternal("AI_TurnToWP", AI_TurnToWP,
-          zPAR_TYPE_VOID, zPAR_TYPE_INSTANCE, zPAR_TYPE_STRING, 0);
-      // func string AI_TurnToVob(var C_NPC npc, var string vobName)
-      parser->DefineExternal("AI_TurnToVob", AI_TurnToVob,
-          zPAR_TYPE_VOID, zPAR_TYPE_INSTANCE, zPAR_TYPE_STRING, 0);
+      // func void AI_TurnToWP(var C_NPC npc, var string wp)
+      parser->DefineExternal("AI_TurnToWP", AI_TurnToWP, VOID, INST, STR, 0);
+      // func void AI_TurnToVob(var C_NPC npc, var string vobName)
+      parser->DefineExternal("AI_TurnToVob", AI_TurnToVob, VOID, INST, STR, 0);
 
       //---------------------------------------------------------------------
       // Other
       //---------------------------------------------------------------------
 
-      parser->DefineExternal("GetIntArrayValue", Ext_ReadStatArray<int>, 
-          zPAR_TYPE_INT, zPAR_TYPE_INT, zPAR_TYPE_INT, 0);
-      parser->DefineExternal("GetStringArrayValue", Ext_ReadStatArray<zSTRING>, 
-          zPAR_TYPE_STRING, zPAR_TYPE_STRING, zPAR_TYPE_INT, 0);
-      parser->DefineExternal("SetIntArrayValue", Ext_WriteStatArray<int>, 
-          zPAR_TYPE_VOID, zPAR_TYPE_INT, zPAR_TYPE_INT, zPAR_TYPE_INT, 0);
-      parser->DefineExternal("SetStringArrayValue", Ext_WriteStatArray<zSTRING>, 
-          zPAR_TYPE_VOID, zPAR_TYPE_STRING, zPAR_TYPE_INT, zPAR_TYPE_STRING, 0);
+      parser->DefineExternal("GetIntArrayValue", Ext_ReadStatArray<int>, INT, INT, INT, 0);
+      parser->DefineExternal("GetStringArrayValue", Ext_ReadStatArray<zSTRING>, STR, STR, INT, 0);
+      parser->DefineExternal("SetIntArrayValue", Ext_WriteStatArray<int>, VOID, INT, INT, INT, 0);
+      parser->DefineExternal("SetStringArrayValue", Ext_WriteStatArray<zSTRING>, VOID, STR, INT, STR, 0);
 
       // var string Str_ToUpperCase(var string str)
-      parser->DefineExternal("Str_ToUpperCase", Str_ToUpperCase, 
-          zPAR_TYPE_STRING, zPAR_TYPE_STRING, 0);
+      parser->DefineExternal("Str_ToUpperCase", Str_ToUpperCase, STR, STR, 0);
       // var string Str_ToLowerCase(var string str)
-      parser->DefineExternal("Str_ToLowerCase", Str_ToLowerCase,
-          zPAR_TYPE_STRING, zPAR_TYPE_STRING, 0);
+      parser->DefineExternal("Str_ToLowerCase", Str_ToLowerCase, STR, STR, 0);
       // var string Str_ToCapitelCase(var string str)
-      parser->DefineExternal("Str_ToCapitelCase", Str_ToCapitelCase,
-          zPAR_TYPE_STRING, zPAR_TYPE_STRING, 0);
+      parser->DefineExternal("Str_ToCapitelCase", Str_ToCapitelCase, STR, STR, 0);
+
+#undef INT
+#undef FLT
+#undef INST
+#undef FUNC
+#undef STR
+
   }
 
   void Game_ApplyOptions() {
